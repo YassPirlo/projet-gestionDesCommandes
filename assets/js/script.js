9f5d909998d3ebf344bd02086c93ab76273dab84
YassV <yassine.vitullo@hotmail.com>
2026-05-12 19:07:08 +0200

    feat: add form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,46 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+let nomErreur = document.getElementById('nom-erreur');
+let prixErreur = document.getElementById('prix-erreur');
+let submitErreur = document.getElementById('form-erreur');
+
+ function validationNom(){
+    let name = document.getElementById('nom').value;
+
+    if (name.length == 0){
+            nomErreur.innerHTML = "le nom est obligatoire";
+            return false
+    }
+    if (!name.match(/^[A-Za-zÀ-ÿ0-9\s'-]{2,50}$/)) {
+      nomErreur.innerHTML = "Ecrire un nom correct"
+      return false
+      }
+
+    nomErreur.innerHTML='';
+    return true;
+ }
+
+
+function validationPrix(){
+  let prix=document.getElementById('prix');
+
+  if (prix.value > 100){
+    prixErreur.innerHTML = "Prix trop élevé";
+    return false;
+
+  }
+  prixErreur.innerHTML="";
+  return true; 
+
+}
+
+function validateForm(){
+  if(!validationNom() ||!validationPrix() ) {
+    submitErreur.innerHTML = "corriger l'erreur pour envoyer le formulaire";
+    setTimeout(function(){submitErreur.style.display='none';},3000);
+    return false;
+  }
+}
+
